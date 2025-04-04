1e4e62de650a6a935326c6dbe7d5dbfcadb15ed9
vsmorodina <v3ra.smorodina@yandex.ru>
2025-04-05 00:23:08 +0300

    version up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     namespace = "com.vsmorodina.myrecipes"
     compileSdk = 35
 
-    val version = 4
+    val version = 5
 
     defaultConfig {
         applicationId = "com.vsmorodina.myrecipes"
